faa4383dc3cbc00cf1e4257b1516511f20fd1d74
김담율 <s24001@gsm.hs.kr>
2024-09-12 20:12:51 +0900

    search box

--- lib/MainPage/main_page.dart
+++ lib/MainPage/main_page.dart
@@ -34,13 +34,56 @@ class MainPage extends StatelessWidget {
             left: -10,
             child: Container(
               decoration: const BoxDecoration(
-                color: Color(0XffF4F9FF),
+                color: Color(0xFFF4F9FF),
                 borderRadius: BorderRadius.all(
                   Radius.circular(10),
                 ),
               ),
               height: 81,
               width: 331,
+            ),
+          ),
+          const Positioned(
+            top: 110,
+            left: 30,
+            child: Text(
+              "Search",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 140,
+            left: 100,
+            child: Container(
+              width: 200,
+              height: 25, // 컨테이너 높이 유지
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.black12,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  isCollapsed: true,
+                  contentPadding: EdgeInsets.only(left: 8, right: 8),
+                  border: InputBorder.none,
+                  suffixIcon: Icon(
+                    Icons.search,
+                    size: 18,
+                  ),
+                ),
+                style: TextStyle(fontSize: 14),
+                textAlignVertical: TextAlignVertical.center,
+              ),
             ),
           ),
         ],
