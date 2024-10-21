b1f3eb122af4ff7337bdb212a78d349c538f994d
ysl <s24012@gsm.hs.kr>
2024-10-21 11:53:12 +0900

    modified:   lib/MainPage/main_page.dart 	modified:   lib/ProfilePage/profile_page.dart 	modified:   lib/main.dart

--- lib/MainPage/main_page.dart
+++ lib/MainPage/main_page.dart
@@ -10,20 +10,12 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final TextEditingController _textController = TextEditingController();
-  int _selectedIndex = 0; // 현재 선택된 네비게이션 아이템 인덱스
 
   // 입력된 값을 저장하는 메서드
   void _saveInputValue() {
     String inputValue = _textController.text;
     print('입력된 값: $inputValue'); // db 연결
     _textController.clear();
-  }
-
-  // 네비게이션 아이템이 선택될 때 호출되는 메서드
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index; // 선택된 아이템의 인덱스를 업데이트
-    });
   }
 
   @override
@@ -39,27 +31,6 @@ class _MainPageState extends State<MainPage> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Icon(
-              Icons.menu_sharp, // 나중에 추가 기능 구현(누르면 탭 나오는 기능)
-              size: screenWidth * 0.08, // 반응형 크기
-            ),
-            SizedBox(
-              width: screenWidth * 0.03, // 반응형 여백
-            ),
-            Text(
-              'NEXUS',
-              style: TextStyle(
-                fontSize: screenWidth * 0.05, // 반응형 폰트 크기
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ],
-        ),
-      ),
       body: Stack(
         children: [
           // 반응형으로 Container 배치
@@ -143,60 +114,6 @@ class _MainPageState extends State<MainPage> {
             ),
           ),
         ],
-      ),
-      // 하단 네비게이션 바 추가
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/image/home.svg',
-              height: screenHeight * 0.03, // 반응형 아이콘 높이
-              width: screenWidth * 0.07, // 반응형 아이콘 너비
-              color: _selectedIndex == 0
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey, // 선택에 따른 색상 변경
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/image/project.svg',
-              height: screenHeight * 0.03,
-              width: screenWidth * 0.07,
-              color: _selectedIndex == 1
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey,
-            ),
-            label: 'Project',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/image/project.svg',
-              height: screenHeight * 0.03,
-              width: screenWidth * 0.07,
-              color: _selectedIndex == 2
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey,
-            ),
-            label: 'Project',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-              'assets/image/profile.svg',
-              height: screenHeight * 0.03,
-              width: screenWidth * 0.07,
-              color: _selectedIndex == 3
-                  ? Theme.of(context).primaryColor
-                  : Colors.grey,
-            ),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex, // 선택된 아이템 인덱스
-        selectedItemColor: Theme.of(context).primaryColor, // 선택된 아이템 색상 통일
-        unselectedItemColor: Colors.grey, // 미선택 아이템 색상
-        onTap: _onItemTapped, // 아이템 선택 시 호출될 메서드
-        type: BottomNavigationBarType.fixed, // 아이템 수가 3개 이상일 때 필요
       ),
     );
   }
--- lib/ProfilePage/profile_page.dart
+++ lib/ProfilePage/profile_page.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Profile Page"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: const Color(0x00f4f9ff),
+            height: 300,
+            width: 350,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 130,
+              horizontal: 20,
+            ),
+          ),
+          SvgPicture.asset(
+            'image/profile.svg',
+            height: 200,
+            width: 200,
+          ),
+        ],
+      ),
     );
   }
 }
