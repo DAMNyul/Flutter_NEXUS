59b8db3ebdd62fc88f01c1f5c8c5712085248951
ysl <s24012@gsm.hs.kr>
2024-10-21 10:55:00 +0900

    new file:   devtools_options.yaml 	new file:   lib/PostPage/post_page.dart 	new file:   lib/ProfilePage/profile_page.dart 	new file:   lib/ProjectPage/project_page.dart 	modified:   lib/main.dart 	new file:   lib/provider/provider.dart 	modified:   pubspec.lock s	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_nexus/MainPage/main_page.dart';
+import 'package:flutter_nexus/PostPage/post_page.dart';
+import 'package:flutter_nexus/ProfilePage/profile_page.dart';
+import 'package:flutter_nexus/ProjectPage/project_page.dart';
+import 'package:flutter_nexus/provider/provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => CurrentPageProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -10,8 +19,89 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MainPage(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: const Header(),
+        body: Consumer<CurrentPageProvider>(
+          builder: (context, currentPageProvider, child) {
+            return [
+              const MainPage(),
+              const ProjectPage(),
+              const PostPage(),
+              const ProfilePage(),
+            ][currentPageProvider.currentPage];
+          },
+        ),
+        bottomNavigationBar: const FootNavigator(),
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget implements PreferredSizeWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return AppBar(
+      title: Row(
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.menu),
+          ),
+          Text(
+            'Nexus',
+            style: theme.textTheme.titleMedium,
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class FootNavigator extends StatefulWidget {
+  const FootNavigator({super.key});
+
+  @override
+  _FootNavigatorState createState() => _FootNavigatorState();
+}
+
+class _FootNavigatorState extends State<FootNavigator> {
+  @override
+  Widget build(BuildContext context) {
+    // final ThemeData theme = Theme.of(context);
+    return NavigationBar(
+      destinations: [
+        NavigationDestination(
+          icon: SvgPicture.asset('image/home.svg'),
+          label: '',
+        ),
+        NavigationDestination(
+          icon: SvgPicture.asset('image/project.svg'),
+          label: '',
+        ),
+        NavigationDestination(
+          icon: SvgPicture.asset('image/chat.svg'),
+          label: '',
+        ),
+        NavigationDestination(
+          icon: SvgPicture.asset(
+            width: 45,
+            'image/profile.svg',
+          ),
+          label: '',
+        )
+      ],
+      onDestinationSelected: (index) {
+        Provider.of<CurrentPageProvider>(context, listen: false)
+            .setCurrentPage(index);
+      },
+      selectedIndex: Provider.of<CurrentPageProvider>(context).currentPage,
     );
   }
 }
