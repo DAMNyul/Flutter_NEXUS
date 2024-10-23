435adca6ccec155fcd3c910fe72c762b9a52c23b
김담율 <s24001@gsm.hs.kr>
2024-10-23 10:13:58 +0900

    FullPage Icon

--- lib/ProjectPage/full_page.dart
+++ lib/ProjectPage/full_page.dart
@@ -1,6 +1,5 @@
-// next_page.dart
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FullPage extends StatelessWidget {
   const FullPage({super.key});
@@ -8,6 +7,11 @@ class FullPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
+    final screenSize = MediaQuery.of(context).size;
+    final double boxSize = screenSize.width * 0.25;
+    final double topPosition = screenSize.height * 0.1;
+    final double leftPosition = screenSize.width * 0.07;
+
     return Scaffold(
       appBar: AppBar(
         title: Row(
@@ -22,9 +26,29 @@ class FullPage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-              child: SvgPicture.asset(
-            'image/project.svg',
-          )),
+            top: topPosition,
+            left: leftPosition,
+            child: SizedBox(
+              width: boxSize,
+              height: boxSize,
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  color: Color(0xFFFFF7E3),
+                ),
+                child: Center(
+                  child: SvgPicture.asset(
+                    'image/project.svg',
+                    color: const Color(0xffFF9900),
+                    width: boxSize * 0.65,
+                    height: boxSize * 0.65,
+                  ),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
