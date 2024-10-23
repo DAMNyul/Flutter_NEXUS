00d82869fb28f8dfb5725cdd998d3f2d7bfa2e53
ysl <s24012@gsm.hs.kr>
2024-10-23 20:48:57 +0900

    modified:   lib/MainPage/main_page.dart

--- lib/MainPage/main_page.dart
+++ lib/MainPage/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -111,146 +113,191 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 90,
-        ),
-        Stack(
-          children: [
-            Transform.translate(
-              offset: const Offset(-65, 0),
-              child: Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 200,
-                ),
-                child: Transform.translate(
-                  offset: const Offset(120, 60),
-                  child: const Icon(
-                    Icons.circle,
-                    color: Color(0xffFFF5DB),
-                    size: 150,
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 20,
-              ),
-              decoration: const BoxDecoration(
-                color: Color(0xffF4F9FF),
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(15),
-                  topRight: Radius.circular(15),
-                ),
-              ),
-              width: 315,
-              height: 120,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Search',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 15),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Colors.white,
-                    ),
-                    width: 250,
-                    height: 45,
-                    child: TextField(
-                      controller: _textController,
-                      decoration: const InputDecoration(
-                        hintText: "Search",
-                        contentPadding: EdgeInsets.only(left: 8, right: 8),
-                        border: InputBorder.none,
-                        suffixIcon: Icon(
-                          Icons.search,
-                          size: 18,
-                        ),
-                      ),
-                      style: const TextStyle(fontSize: 14),
-                      textAlignVertical: TextAlignVertical.center,
-                      onChanged: (value) {
-                        setState(() {
-                          userInput = value; // 사용자가 입력하는 텍스트를 저장
-                        });
-                      },
-                      onSubmitted: (value) {
-                        _saveInputValue(); // 입력값을 저장하고 다른 처리를 수행
-                        _textController.clear(); // 입력 필드 초기화
-                        setState(
-                          () {
-                            userInput = ""; // 입력 값을 초기화
-                          },
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(
-          height: 100,
-        ),
-        Transform.translate(
-          offset: const Offset(-100, 0),
-          child: Stack(
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
             children: [
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(30),
-                    topRight: Radius.circular(30),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 85),
+                child: Positioned(
+                  height: MediaQuery.of(context).size.height * 0.325,
+                  child: Container(
+                    clipBehavior: Clip.hardEdge,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                    ),
+                    width: 350,
+                    height: 150,
+                    child: Transform.translate(
+                      offset: const Offset(160, 65),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Color(0xffFFF5DB),
+                        ),
+                        width: 120,
+                      ),
+                    ),
                   ),
-                  color: Color(0xffF4F9FF),
                 ),
-                width: 210,
-                height: 300,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 10,
+              ),
+              Positioned(
+                height: MediaQuery.of(context).size.height * 0.325,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 85),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xffF4F9FF),
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
+                    ),
+                    width: 315,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Search",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            width: MediaQuery.of(context).size.width * 0.6,
+                            child: Container(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 15),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(30),
+                                color: Colors.white,
+                              ),
+                              width: 250,
+                              height: 45,
+                              child: TextField(
+                                controller: _textController,
+                                decoration: const InputDecoration(
+                                  hintText: "Search",
+                                  contentPadding:
+                                      EdgeInsets.only(left: 8, right: 8),
+                                  border: InputBorder.none,
+                                  suffixIcon: Icon(
+                                    Icons.search,
+                                    size: 18,
+                                  ),
+                                ),
+                                style: const TextStyle(fontSize: 14),
+                                textAlignVertical: TextAlignVertical.center,
+                                onChanged: (value) {
+                                  setState(() {
+                                    userInput = value; // 사용자가 입력하는 텍스트를 저장
+                                  });
+                                },
+                                onSubmitted: (value) {
+                                  _saveInputValue(); // 입력값을 저장하고 다른 처리를 수행
+                                  _textController.clear(); // 입력 필드 초기화
+                                  setState(
+                                    () {
+                                      userInput = ""; // 입력 값을 초기화
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
+                ),
+              ),
+              const SizedBox(
+                height: 120,
+              ),
+            ],
+          ),
+          Transform.translate(
+            offset: const Offset(-70, 0),
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  clipBehavior: Clip.none,
+                  decoration: const BoxDecoration(
+                    color: Color(0xffF4F9FF),
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                  ),
+                  width: 210,
+                  height: 270,
                   child: const Column(
                     children: [
-                      Text(
-                        "Best Project",
-                        style: TextStyle(
-                          fontSize: 17.5,
-                          fontWeight: FontWeight.w600,
-                        ),
+                      Row(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 7,
+                              vertical: 20,
+                            ),
+                          ),
+                          Text(
+                            "Best Project",
+                            style: TextStyle(
+                              fontSize: 17.5,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
                       ),
                       SizedBox(
                         height: 10,
                       ),
-                      NexusCard(),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 5),
+                            ),
+                            NexusCard(),
+                            SizedBox(width: 10),
+                            NexusCard(),
+                            SizedBox(width: 10),
+                            NexusCard(),
+                            SizedBox(width: 10),
+                            NexusCard(),
+                            SizedBox(width: 10),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
