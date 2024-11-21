0c445f2002aeaf8546746772dee80a0b817a8153
김담율 <s24001@gsm.hs.kr>
2024-11-21 20:02:43 +0900

    2024.11.21/8:02

--- lib/PostPage/create_post_page.dart
+++ lib/PostPage/create_post_page.dart
@@ -13,27 +13,9 @@ class CreatePostPage extends StatelessWidget {
       ),
       body: Stack(
         children: [
-          // 배경 원형 1
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.05,
-            left: MediaQuery.of(context).size.width * -0.3,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 0.7,
-              height: MediaQuery.of(context).size.width * 0.7,
-              child: Container(
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0XFFFFF7E3),
-                ),
-              ),
-            ),
-          ),
-          // 배경 원형 2
-          // 배경 원형 2
           Positioned(
             top: MediaQuery.of(context).size.height * 0.4,
-            right: MediaQuery.of(context).size.width *
-                -0.15, // 음수 값으로 오른쪽 벽면에 반쯤 붙이기
+            right: MediaQuery.of(context).size.width * 0.1,
             child: SizedBox(
               width: MediaQuery.of(context).size.width * 0.7,
               height: MediaQuery.of(context).size.width * 0.7,
@@ -45,26 +27,38 @@ class CreatePostPage extends StatelessWidget {
               ),
             ),
           ),
-
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              SizedBox(
-                height: 30,
-              ),
-              const Text(
-                'Create Post',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                '글을 작성하고 사람들과 공유해보세요',
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Color(0xff757575),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.4,
+            right: MediaQuery.of(context).size.width * -0.3,
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.width * 0.7,
+              child: Container(
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xffFFF7E3),
                 ),
               ),
-            ]),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                const Text(
+                  'Create Post',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ), // 두 텍스트 사이의 간격
+                const Text(
+                  '글을 작성하고 사람들과 공유해보세요',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0xff757575),
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
