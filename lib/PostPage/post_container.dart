94b806bb7750c8095e3f29686c076068885c3990
김담율 <s24001@gsm.hs.kr>
2024-12-12 20:37:18 +0900

    에러 그만 제발 제발 제발 제발 제발 제발 제발 제발 제발 내 앱 디자인 제발 돌려줘요 왜그러는거야 나한테 진짜 이건 아니잖아 제발 나한테 왜 그러는건데

--- lib/PostPage/post_container.dart
+++ lib/PostPage/post_container.dart
@@ -4,8 +4,56 @@ import 'post_detail_page.dart'; // PostDetailPage 경로를 확인하고 추가
 class PostContainer extends StatelessWidget {
   final String title;
   final String content;
+  final String postId; // 포스트의 ID 추가 (삭제 확인용)
+  final String currentUserId; // 현재 사용자 ID 추가
 
-  const PostContainer({super.key, required this.title, required this.content});
+  const PostContainer({
+    super.key,
+    required this.title,
+    required this.content,
+    required this.postId,
+    required this.currentUserId,
+  });
+
+  // 삭제 함수
+  void _deletePost(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('정말 해당 게시물 "$title"을 삭제하시겠습니까?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                '아니요',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                // 포스트 삭제 코드 작성 (삭제 후 리스트 갱신 등)
+                Navigator.of(context).pop(); // 다이얼로그 닫기
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('삭제되었습니다')),
+                );
+
+                // 삭제 후 PostPage로 이동하면서 포스트를 갱신할 수 있도록 전달
+                Navigator.pop(context, postId);
+              },
+              child: const Text(
+                '네',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +76,7 @@ class PostContainer extends StatelessWidget {
             borderRadius: BorderRadius.circular(20),
             boxShadow: [
               BoxShadow(
-                color: Colors.grey.withOpacity(0.7),
+                color: Colors.grey.withOpacity(0.1),
                 spreadRadius: 1,
                 blurRadius: 5,
               ),
@@ -57,6 +105,16 @@ class PostContainer extends StatelessWidget {
                   style: const TextStyle(fontSize: 14),
                 ),
               ),
+              // 삭제 버튼 추가 (오른쪽 상단)
+              if (currentUserId == postId) // 작성자와 사용자가 같을 때만 표시
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: IconButton(
+                    icon: const Icon(Icons.delete, color: Colors.red),
+                    onPressed: () => _deletePost(context),
+                  ),
+                ),
             ],
           ),
         ),
@@ -67,8 +125,10 @@ class PostContainer extends StatelessWidget {
 
 class PartyOfPostContainer extends StatelessWidget {
   final List<Map<String, String>> posts;
+  final String currentUserId; // 현재 사용자 ID
 
-  const PartyOfPostContainer({super.key, required this.posts});
+  const PartyOfPostContainer(
+      {super.key, required this.posts, required this.currentUserId});
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +138,8 @@ class PartyOfPostContainer extends StatelessWidget {
         return PostContainer(
           title: post['title']!,
           content: post['content']!,
+          postId: post['id']!,
+          currentUserId: currentUserId, // 사용자 ID 전달
         );
       }).toList(),
     );
--- lib/PostPage/post_page.dart
+++ lib/PostPage/post_page.dart
@@ -1,89 +1,90 @@
 import 'package:flutter/material.dart';
 import 'create_post_page.dart'; // CreatePostPage 관련 코드 임포트
 import 'post_container.dart'; // PartyOfPostContainer 관련 코드 임포트
+import 'post_detail_page.dart';
+
+// PostPage에서 새로 생성된 포스트를 받는 부분 수정
 
 class PostPage extends StatefulWidget {
-  final List<Map<String, String>> posts = [];
+  final String currentUserId;
 
-  PostPage({super.key});
+  const PostPage({super.key, required this.currentUserId});
 
   @override
   _PostPageState createState() => _PostPageState();
 }
 
 class _PostPageState extends State<PostPage> {
+  List<Map<String, String>> posts = []; // 포스트 목록 상태
+
+  // 포스트 삭제 후 상태 업데이트 함수
+  void _updatePostList(String postId) {
+    setState(() {
+      posts.removeWhere((post) => post['id'] == postId); // 해당 id에 해당하는 포스트 삭제
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Colors.white, // 배경색 하얀색 고정
-        title: const Text(
-          'Posts',
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-        elevation: 0, // 그림자 제거
+        title: const Text("포스트 리스트"),
+        backgroundColor: Colors.white,
       ),
-      body: SafeArea(
-        child: Stack(
-          children: [
-            // 배경 원들
-            Positioned(
-              top: MediaQuery.of(context).size.height * 0.05,
-              left: MediaQuery.of(context).size.width * -0.3,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.7,
-                height: MediaQuery.of(context).size.width * 0.7,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFFFFF7E3),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: MediaQuery.of(context).size.height * 0.05,
-              right: MediaQuery.of(context).size.width * -0.3,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.7,
-                height: MediaQuery.of(context).size.width * 0.7,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color(0xFFFFF7E3),
-                ),
-              ),
-            ),
-            // 메인 컨텐츠
-            widget.posts.isEmpty
-                ? const Center(
-                    child: Text(
-                      'No posts yet.',
-                      style: TextStyle(fontSize: 16, color: Colors.grey),
+      body: FutureBuilder<List<Map<String, String>>>(
+        // 예시로 데이터를 FutureBuilder로 비동기 처리했다고 가정
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return const Center(child: Text("에러 발생"));
+          }
+
+          final posts = snapshot.data ?? []; // null 체크 추가
+          return ListView.builder(
+            itemCount: posts.length,
+            itemBuilder: (context, index) {
+              final post = posts[index];
+              return ListTile(
+                title: Text(post['title'] ?? '제목 없음'), // null 체크 추가
+                subtitle: Text(post['content'] ?? '내용 없음'), // null 체크 추가
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PostDetailPage(
+                        title: post['title'] ?? '제목 없음', // null 체크 추가
+                        content: post['content'] ?? '내용 없음', // null 체크 추가
+                      ),
                     ),
-                  )
-                : PartyOfPostContainer(posts: widget.posts),
-          ],
-        ),
+                  );
+                },
+              );
+            },
+          );
+        },
+        future: Future.value(posts), // 실제 데이터를 Future로 전달
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xffF4F9FF),
         onPressed: () async {
-          // CreatePostPage에서 새 포스트 데이터를 받아옴
+          // 포스트 추가 페이지로 이동
           final newPost = await Navigator.push<Map<String, String>>(
             context,
-            MaterialPageRoute(
-              builder: (context) => const CreatePostPage(),
-            ),
+            MaterialPageRoute(builder: (context) => const CreatePostPage()),
           );
 
-          // 전달된 데이터가 null이 아닌 경우 상태 업데이트
+          // 새로 생성된 포스트가 null이 아닐 때만 추가
           if (newPost != null) {
             setState(() {
-              widget.posts.add(newPost);
+              posts.add(newPost); // 포스트 목록에 새 포스트 추가
             });
           }
         },
-        backgroundColor: const Color(0xFFDAE9FF),
-        child: const Icon(Icons.add, color: Colors.white),
+        child: const Icon(Icons.add),
       ),
     );
   }
