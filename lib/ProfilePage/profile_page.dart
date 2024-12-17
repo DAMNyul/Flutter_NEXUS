6c4ae6c99768e5969ec88d6c0e46c559f68e357a
ysl <s24012@gsm.hs.kr>
2024-12-17 12:22:55 +0900

    add follow and message button in profile page

--- lib/ProfilePage/profile_page.dart
+++ lib/ProfilePage/profile_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  String onClick() {
+    return "버튼 클릭";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,148 +29,178 @@ class ProfilePage extends StatelessWidget {
                 ),
                 color: Color(0xfff4f9ff),
               ),
-              height: 400,
-              width: 350,
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 25),
-                  ),
-                  const Text(
-                    "Username",
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Transform.translate(
-                        offset: const Offset(60, 20),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            color: Color(0xfff4f9ff),
+              height: MediaQuery.of(context).size.height * 0.5,
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Username",
+                            style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                          child: const Column(
-                            children: [
-                              Text(
-                                "Follower",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.55,
+                            child: Row(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: onClick,
+                                  child: Text("Follow"),
                                 ),
-                              ),
-                              Text(
-                                "1",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w500,
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.05,
                                 ),
-                              )
-                            ],
+                                ElevatedButton(
+                                  onPressed: onClick,
+                                  child: Text("Message"),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                      Transform.translate(
-                        offset: const Offset(90, 20),
-                        child: Container(
-                          decoration: const BoxDecoration(
-                            color: Color(0xfff4f9ff),
-                          ),
-                          child: const Column(
-                            children: [
-                              Text(
-                                "Following",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              Text(
-                                "10000",
-                                style: TextStyle(
-                                  fontSize: 17,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Transform.translate(
-                    offset: const Offset(-50, 90),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: const Color(0x5fe76f00),
-                          ),
-                          width: 100,
-                          height: 40,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                'image/java.svg',
-                                width: 27,
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              const Text(
-                                "Java",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Color(0xffe76f00),
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(30),
-                            color: const Color(0x5f70AD51),
-                          ),
-                          width: 150,
-                          height: 40,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              SvgPicture.asset(
-                                'image/spring.svg',
-                                width: 27,
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              const Text(
-                                "Spring",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Color(0xff70AD51),
-                                  fontWeight: FontWeight.w400,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ],
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xfff4f9ff),
+                            ),
+                            child: const Column(
+                              children: [
+                                Text(
+                                  "Follower",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  "1",
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            width: MediaQuery.of(context).size.width * 0.1,
+                          ),
+                          Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xfff4f9ff),
+                            ),
+                            child: const Column(
+                              children: [
+                                Text(
+                                  "Following",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  "10000",
+                                  style: TextStyle(
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.03,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.6,
+                      height: MediaQuery.of(context).size.height * 0.15,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: const Color(0x5fe76f00),
+                            ),
+                            width: 100,
+                            height: 40,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(
+                                  'image/java.svg',
+                                  width: 27,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Text(
+                                  "Java",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xffe76f00),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: const Color(0x5f70AD51),
+                            ),
+                            width: 150,
+                            height: 40,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(
+                                  'image/spring.svg',
+                                  width: 27,
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Text(
+                                  "Spring",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xff70AD51),
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
